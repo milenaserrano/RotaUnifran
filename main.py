459a549e0eab86e6ad95a83e95606630d5e896d5
Milena <68025219+milenaserrano@users.noreply.github.com>
2022-03-17 22:48:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/') 
+def hello_world():
+  return '<h1>Olá Mundo!</h1>'
+
+@app.route('/unifran') 
+def unifran():
+  return '<h1>Universidade de Franca</h1>'
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
